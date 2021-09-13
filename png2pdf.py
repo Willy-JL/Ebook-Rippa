d35ffeee4ece62bff9bbdcc51ec512831ed15b8b
Willy-JL <willy.leslie@icloud.com>
2021-09-13 22:44:06 +0200

    Refactor + snake case

--- png2pdf.py
+++ png2pdf.py
@@ -5,30 +5,34 @@ import sys
 import os
 
 
-def makePdf(pdfFileName, listPages):
-    cover = Image.open(str(listPages[0]) + '.png')
+def make_pdf(output_filename, img_list):
+    cover = Image.open(str(img_list[0]) + '.png')
     width, height = cover.size
     print(f'Identified page sizing: {width}x{height}')
 
     pdf = FPDF(unit='pt', format=[width, height])
 
-    for j, page in enumerate(listPages):
+    for j, page in enumerate(img_list):
         name = str(page) + '.png'
         pdf.add_page()
         pdf.image(name, 0, 0)
         print(f'Added file \"{name}\", page number {j+1}')
 
     print('Saving output PDF...')
-    pdf.output(pdfFileName + '.pdf', 'F')
+    pdf.output(output_filename, 'F')
 
 
 if __name__ == '__main__':
-    pngList = glob.glob('*.png')
-    for i, file in enumerate(pngList):
-        pngList[i] = int(file[:-4])
-    pngList = sorted(pngList)
-    print(f'Found {len(pngList)} images to process...')
-    dirName = os.path.dirname(os.path.realpath(__file__))[os.path.dirname(os.path.realpath(__file__)).rfind('\\' if sys.platform.startswith("win") else "/")+1:]
-    print(f'Starting to compile \"{dirName}.pdf\"')
-    makePdf(dirName, pngList)
+    png_list = glob.glob('*.png')
+    for i, filename in enumerate(png_list):
+        png_list[i] = int(filename[:-4])
+    png_list = sorted(png_list)
+    print(f'Found {len(png_list)} images to process...')
+    script_path = os.path.realpath(__file__)
+    parent_folder = os.path.dirname(script_path)
+    os.chdir(parent_folder)
+    parent_folder = parent_folder.replace("\\", "/")
+    pdf_filename = parent_folder[parent_folder.rfind("/")+1:] + ".pdf"
+    print(f'Starting to compile \"{pdf_filename}\"')
+    make_pdf(pdf_filename, png_list)
     input('All Done!')
